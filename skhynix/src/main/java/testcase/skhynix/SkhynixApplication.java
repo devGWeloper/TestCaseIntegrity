787101194861d2019ecd17660b16cd0b69cee70e
devGWeloper <53815271+devGWeloper@users.noreply.github.com>
2024-09-06 20:02:42 +0900

    initial commit

---
+++ skhynix/src/main/java/testcase/skhynix/SkhynixApplication.java
@@ -0,0 +1,13 @@
+package testcase.skhynix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhynixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkhynixApplication.class, args);
+	}
+
+}
---
+++ skhynix/src/test/java/testcase/skhynix/SkhynixApplicationTests.java
@@ -0,0 +1,13 @@
+package testcase.skhynix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhynixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
